71a8c9eb8a7012f4f11fdf0064f49af7a4e519b1
Max Dietrich <mail@mxd.codes>
2022-05-08 18:29:07 +0200

    preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   reactStrictMode: true,
+    webpack(config, { dev, isServer }) {
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          "react": "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+          "react-dom": "preact/compat",
+        })
+      }
+      /*if (isServer) {
+        require('./lib/utils/generate-sitemap');
+      }*/
+      return config
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
 }
