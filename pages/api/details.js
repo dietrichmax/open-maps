ca437ce7aa10f1d40551747d9718a393786b7ac8
Max Dietrich <mail@mxd.codes>
2022-05-20 10:24:09 +0200

    asd

--- pages/api/details.js
+++ pages/api/details.js
@@ -104,8 +104,8 @@ export default async function handler(req, res) {
       opening_hours: geocodingData[0].extratags.opening_hours,
       internet_access: geocodingData[0].extratags.internet_access,
     },
-    image: imageUrl,
-    summary: summary,
+    //image: imageUrl,
+    summary,
     wikipediaLang: wikiLang,
     wikipediaLink: wikipediaLink,
   })
